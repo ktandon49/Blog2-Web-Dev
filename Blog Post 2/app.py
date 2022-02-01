dfd8c247267f99d44090089e0c88e872b16bb02e
ktandon49 <tandon.kartik@gmail.com>
2022-02-01 11:37:29 -0800

    added get_message_db function

--- Blog Post 2/app.py
+++ Blog Post 2/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
+import sqlite3
+
+def get_message_db():
+    if "message_db" not in g:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+    
+    g.message_db.execute('CREATE TABLE messages (id INTEGER, handle TEXT, message TEXT)')
+    
+    return g.message_db
+
 #Base page is linked to base.html
 @app.route("/")
 def main():
